0fb9c16691b55e285b8882222bdb89f3585c0b60
Pedr-1n <117911345+Pedr-1n@users.noreply.github.com>
2022-12-16 10:59:54 -0300

    Add files via upload

---
+++ e e ou.py
@@ -0,0 +1,8 @@
+resposta=int( input('Qual sua idade: ') )
+deficiente = str (input (" possui deficiencia ?"))
+if resposta>=18 and or resposta <=65 
+    print('Você é obrigado a votar!')
+    
+else:
+
+    print('Você não é obrigado a votar')
